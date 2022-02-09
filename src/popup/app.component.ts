3be8b4fadefdbd920792fcc31d6cb30ae892b5ba
Thomas Rittson <trittson@bitwarden.com>
2022-02-09 13:24:21 +1000

    Fix typo

--- src/popup/app.component.ts
+++ src/popup/app.component.ts
@@ -119,7 +119,7 @@ export class AppComponent implements OnInit {
     };
 
     // Private mode runs the background in the popup and doesn't use listeners
-    if (!this.popupUtilsService.inPrivateMode) {
+    if (!this.popupUtilsService.inPrivateMode()) {
       BrowserApi.messageListener(
         "app.component",
         (window as any).bitwardenPopupMainMessageListener
